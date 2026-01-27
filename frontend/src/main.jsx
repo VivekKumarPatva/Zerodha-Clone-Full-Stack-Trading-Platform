fa35396a5650d1d5b02f78a0894327e88d951b68
VivekKumarPatva <vivekkumarpatva@gmail.com>
2026-01-27 18:15:49 +0530

    build AboutPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
@@ -9,6 +10,7 @@ import ProductPage from "./landing_page/products/ProductPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
 import Navbar from "./landing_page/Navbar";
+
 import Footer from "./landing_page/Footer";
 import NotFound from "./landing_page/NotFound";
 
